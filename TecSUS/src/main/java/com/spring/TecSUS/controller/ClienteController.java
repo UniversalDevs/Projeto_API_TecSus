c9b2a4f97e7b86f582da66d5ed7329fa8d2ef01c
FelipeASousa <feaugusto893@gmail.com>
2022-04-11 17:17:52 -0300

    Lista de Cliente, metodo get cliente

--- TecSUS/src/main/java/com/spring/TecSUS/controller/ClienteController.java
+++ TecSUS/src/main/java/com/spring/TecSUS/controller/ClienteController.java
@@ -7,6 +7,7 @@ import com.spring.TecSUS.service.ClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,4 +25,14 @@ public class ClienteController {
         mv.addObject("clientes", clientes);
         return mv;
     }
+
+    @RequestMapping(value = "/clientes/{cli_id}", method = RequestMethod.GET)
+    public ModelAndView getClienteDetails(@PathVariable("cli_id") long cli_id) {
+        ModelAndView mv = new ModelAndView("clienteDetails");
+        Cliente cliente = clienteService.findById(cli_id);
+        mv.addObject("cliente", cliente);
+        return mv;
+    }
+
+    
 }
