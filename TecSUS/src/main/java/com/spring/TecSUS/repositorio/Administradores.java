2e5d3ef5d0cabddd6fa432d74692471d2165a4ca
amandavo <amandavoliveira0@gmail.com>
2022-05-18 10:25:40 -0300

    cadastro de usuarios

---
+++ TecSUS/src/main/java/com/spring/TecSUS/repositorio/Administradores.java
@@ -0,0 +1,10 @@
+package com.spring.TecSUS.repositorio;
+
+import com.spring.TecSUS.modelo.Administrador;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Administradores extends CrudRepository<Administrador, Integer>{
+    
+
+}
